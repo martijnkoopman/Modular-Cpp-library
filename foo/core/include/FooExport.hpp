fba9c19f90949300b9ed889683cf56aa2682f59a
Martijn Koopman <m_kopie@hotmail.com>
2019-09-05 19:50:57 +0200

    Export for Mac

--- foo/core/include/FooExport.hpp
+++ foo/core/include/FooExport.hpp
@@ -1,12 +1,12 @@
 #if defined(FOO_SHARED)
-#  ifdef __linux__
+#  if defined(__linux__) || defined(__APPLE__)
 #    if defined(FOO_LIBRARY)
 #      define FOO_EXPORT __attribute__((visibility("default")))
 #    else
 #      define FOO_EXPORT
 #    endif
 #  endif
-#  ifdef _WIN32
+#  if defined(_WIN32)
 #    if defined(FOO_LIBRARY)
 #      define FOO_EXPORT __declspec(dllexport)
 #    else
